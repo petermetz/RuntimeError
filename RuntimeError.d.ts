1c09c4b305e886af821e6c7c3ebfe15a2ec29c1a
lsphillips <lsphillips.mail@gmail.com>
2017-12-29 16:49:45 +0000

    Tidied up the TypeScript type definitions. Moved away from the bash scripts. Simplified the .gitignore and .npmignore files. Moved to protect-me-from-my-stupidity as an ESLint standard.

--- RuntimeError.d.ts
+++ RuntimeError.d.ts
@@ -1,15 +1,8 @@
-// Type definitions for RuntimeError
-// --------------------------------------------------------
-
 declare class RuntimeError extends Error
 {
 	cause : Error | null;
 
-	// ----------------------------------------------------
-
 	constructor(message : string, cause? : Error);
-
-	// ----------------------------------------------------
 
 	toJSON() : Object;
 }
